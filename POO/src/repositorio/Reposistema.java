d267e3cce290456385145aa658c7c2e822ff56bd
ADELMIR NEVES FERREIRA JUNIOR <adelmir.nfjunior@senacsp.edu.br>
2022-06-21 08:20:18 -0300

    Programacao Orientada a Objetos

---
+++ POO/src/repositorio/Reposistema.java
@@ -0,0 +1,38 @@
+package repositorio;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import classes.PrjCliente;
+import classes.PrjUsuario;
+import classes.PrjEndereco;
+
+public class Reposistema {
+	List<PrjUsuario> lstUsuario = new ArrayList<PrjUsuario>();
+	List<PrjEndereco> lstEndereco = new ArrayList<PrjEndereco>();
+	List<PrjCliente> lstCliente = new ArrayList<PrjCliente>();
+	
+	public String cadUsuario(PrjUsuario usuario) {
+		lstUsuario.add(usuario);
+		return "Usuário cadastrado";
+		
+	}
+	public List<PrjUsuario> listarUsuario(){
+		return lstUsuario;
+	}
+	public String cadEndereco(PrjEndereco endereco) {
+		lstEndereco.add(endereco);
+		return "Endereço cadastrado";
+	}
+	public List<PrjEndereco> listarEndereco(){
+		return lstEndereco;
+	}
+	public String cadCliente(PrjCliente cliente) {
+		lstCliente.add(cliente);
+		return "Cliente cadastrado";
+	}
+	public List<PrjCliente> listarCliente(){
+		return lstCliente;
+	}
+	
+}
